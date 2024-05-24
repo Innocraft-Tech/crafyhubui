c9394f2c47947dcbb6845d90ba0ec4c6887396a5
Tamil-10 <stamil1090@gmail.com>
2024-05-25 01:55:19 +0530

    fix

--- src/app/(user)/discover/page.tsx
+++ src/app/(user)/discover/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Filters } from './components/filters';
 import { DiscoverCard } from './components/card';
-import { Signal } from 'lucide-react';
 
 export default function Discover() {
   return (
